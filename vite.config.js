bc9d7a0fb8c5d6658aab2e161831ad33b4038c95
richdoor <richardamador26@gmail.com>
2025-10-05 20:40:35 -0700

    install and integrate tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/fall-2025-website/'
 })
