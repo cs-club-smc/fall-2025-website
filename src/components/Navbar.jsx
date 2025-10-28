746241a3ea9fbea5ff6486b61b5247cf66a80d65
Justin <juhsztn@gmail.com>
2025-10-27 20:26:07 -0700

    Navigation Buttons
    Made an unordered list of buttons that will function as a directory for the site.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+function navbar() {
+    return(
+        <nav>
+            <ul>
+                <li><a href = "#">Home</a></li>
+                <li><a href = "#">About</a></li>
+                <li><a href = "#">Officers</a></li>
+                <li><a href = "#">Join Now</a></li>
+            </ul>
+        </nav>
+    )
+}
