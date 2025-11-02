c62b8ff02c497cd7a6cae0bfdae3fe8b6e5a61e8
Aidana <kudaibergenova_aid01@student.smc.edu>
2025-11-01 20:25:04 -0700

    about.jsx add background and some text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,35 @@
+import background from '../assets/About/background.png'
 
 function About() {
     return(
-        <>
-            <h1>About page</h1>
-        </>
+       <main
+        style={{
+            minHeight: '100vh',
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+        }}
+       >
+        <h1>ABOUT US</h1>
+
+        <p>Description Of the Club</p>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
+         
+        <p>Mission Statement </p>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p> 
+          
+        <p>Goals</p>  
+        <p>"Lorem ipsum dolor sit amet afowaifawi aofiwafioafafaoifafoafaifbiawofbao foaifbwoaa teoaifbwaoi  </p>
+         
+        <p>Advisors </p>
+        <p>"Lorem ipsum dolor sit amet </p>
+
+        <p>Affiliated Organizations</p>
+        <p>"Lorem ipsum dolor </p> 
+
+       </main>
     )
 }
 
